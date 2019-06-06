9355d5caeda171ba9e5ae850558fc2a5ca0895b1
Shindo <cn.shindo@gmail.com>
2019-06-05 17:58:17 -0700

    Add partial unit test in c#

---
+++ heatshrink-unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("heatshrink-unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("heatshrink-unittest")]
+[assembly: AssemblyCopyright("Copyright © Shindo 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ea9ba13-50b0-4484-89f6-ffa1d968bd56")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
